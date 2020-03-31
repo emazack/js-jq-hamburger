8d2c1f0f04a530cfa31021ae30a9f03a3c3f9e93
Emanuele <em.zaccaria@gmail.com>
2020-03-31 15:41:44 +0200

    Starting point

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 // Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript/jQuery
 // prendo lo zip che dopo vi allego e senza toccare l’HTML faccio funzionare l’hamburger menù;
 // primo click, appare il menù;
-// se cliccko che è già visibile, invece scompare.
+// se cliccko che è già visibile, invece scompare
